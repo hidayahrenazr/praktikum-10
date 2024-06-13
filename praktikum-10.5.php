8b2db55ddfb339590e1e01b3a6789df178266ef4
unknown <hidayahrenazr@gmail.com>
2024-06-13 14:52:58 +0700

    Setup Project Praktikum 10

---
+++ praktikum-10.5.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Praktikum 10</title>
+</head>
+<body>
+    <?php
+    $string1 = "Pemgrograman";
+    $string2 = "Web Lanjut";
+    $string3 = "Semester Ganjil";
+
+    echo $string1 . " " . $string2 . " " . $string3;
+
+    ?>
+</body>
+</html>
